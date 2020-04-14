ed3462f33009771326d164526a53dba524127d5c
Jordy Manner <jordy.manner@milkcreation.fr>
2020-04-14 13:29:48 +0000

    search -- 2.0 -- Version fonctionnelle

--- SearchServiceProvider.php
+++ SearchServiceProvider.php
@@ -3,41 +3,23 @@
 namespace tiFy\Plugins\Search;
 
 use tiFy\Container\ServiceProvider;
-use tiFy\Plugins\Search\Wordpress\Wordpress;
+use tiFy\Plugins\Search\Wordpress\WpSearch;
 
 class SearchServiceProvider extends ServiceProvider
 {
-    /**
-     * Liste des noms de qualification des services fournis.
-     * @internal requis. Tous les noms de qualification de services à traiter doivent être renseignés.
-     * @var string[]
-     */
-    protected $provides = [
-        'search',
-        'search.wordpress'
-    ];
-
     /**
      * @inheritDoc
      */
     public function boot(): void
     {
-        $this->getContainer()->share('search.wordpress', function() {
-            return new Wordpress($this->getContainer());
-        });
-
         if (($wp = $this->getContainer()->get('wp')) && $wp->is()) {
-            $this->getContainer()->get('search.wordpress');
-        }
-    }
+            $this->getContainer()->share('search', new WpSearch($this->getContainer()));
 
-    /**
-     * @inheritDoc
-     */
-    public function register(): void
-    {
-        $this->getContainer()->share('search', function() {
-            return (new Search($this->getContainer()));
-        });
+            add_action('after_setup_theme', function () {
+                $this->getContainer()->get('search')->build();
+            });
+        } else {
+            $this->getContainer()->share('search', new Search($this->getContainer()));
+        }
     }
 }
