6f5df39f1cac0ce4f69df04fcb1b1d0f3d7e0ed5
5upp0r7 <5upp0r7@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-08-24 08:54:40 +0000

    search -- Compatibilité restatic 2.0

--- search/branches/2.0/Proxy/Search.php
+++ search/branches/2.0/Proxy/Search.php
@@ -26,7 +26,7 @@ class Search extends AbstractProxy
     /**
      * @inheritDoc
      */
-    public static function getInstanceIdentifier()
+    public static function getInstanceIdentifier(): string
     {
         return 'search';
     }
